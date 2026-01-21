c61d97e25b724d82d5e5995f94d90828618597cf
SamDeiter <sam.deiter@epicgames.com>
2026-01-21 13:00:34 -0500

    docs: add barrel exports and README
    - Create blueprint/index.js barrel export
    - Create material/index.js barrel export
    - Create shared/index.js barrel export
    - Add comprehensive README.md with directory structure
    - All 328 tests passing

---
+++ shared/index.js
@@ -0,0 +1,8 @@
+/**
+ * Shared Utilities - Barrel Exports
+ * Common modules used by both Blueprint and Material editors.
+ */
+
+export { Utils, debounce, generateId } from './utils.js';
+export { WireRenderer } from './WireRenderer.js';
+export { GridRenderer } from './GridRenderer.js';
